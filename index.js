a207cda533375abbaf4410d1e7a791e7257072f9
xotnet <150250186+xotnet@users.noreply.github.com>
2024-09-29 07:48:23 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-const express = require("express");
-const { createProxyMiddleware } = require("http-proxy-middleware");
-require("dotenv").config();
+var proxy = require('redbird')({ port: 8080 });
 
-const app = express();
-
-// Target URL to which the proxy will forward requests
-const targetUrl = process.env.TARGET_URL;
-
-// Proxy all incoming requests to the target URL
-app.use(
-  "/",
-  createProxyMiddleware({
-    target: targetUrl,
-    changeOrigin: true,
-    pathRewrite: {
-      "^/": "/", // Rewrite the path (if needed)
-    },
-    onProxyReq: (proxyReq, req, res) => {
-      console.log(
-        `Proxying request to: ${targetUrl}${req.url} (Method: ${
-          req.method
-        }, Headers: ${JSON.stringify(req.headers)})`
-      );
-    },
-  })
-);
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Proxy server is running on port ${PORT}`);
-});
+proxy.register('optimalbits.com', 'http://167.23.42.67:8000');
